bfbde7af67dd39d4f67cb0462e165ffc8eb2515f
63070054 <63070054@kmitl.ac.th>
2024-03-18 23:30:28 +0700

    add vitge confifg

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [updateCommonjsPlugin()],
   define: {
     global: {},
   },
@@ -16,3 +16,33 @@ export default defineConfig({
     },
   },
 })
+
+
+export const updateCommonjsPlugin = () => {
+  const commonJs22 = commonjs({
+    include: [/node_modules/],
+    extensions: [".js", ".cjs"],
+    strictRequires: true,
+  });
+
+  return {
+    name: "new-common-js",
+    options(rawOptions) {
+      const plugins = Array.isArray(rawOptions.plugins)
+        ? [...rawOptions.plugins]
+        : rawOptions.plugins
+          ? [rawOptions.plugins]
+          : [];
+
+      const index = plugins.findIndex(
+        (plugin) => plugin && plugin.name === "commonjs"
+      );
+      if (index !== -1) {
+        plugins.splice(index, 1, commonJs22);
+      }
+
+      const nextConfig = { ...rawOptions, plugins };
+      return commonJs22.options.call(this, nextConfig);
+    },
+  };
+};
